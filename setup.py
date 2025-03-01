434993312c80c73e3e8759fe5c4d15cdcc3ba0ed
iosman456 <muhammetcanaltan41@gmail.com>
2025-03-01 18:26:01 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,12 @@
-pr
+from setuptools import setup
+
+setup(
+    name='iterminal',
+    version='1.0.0',
+    py_modules=['iterminal'],
+    entry_points={
+        'console_scripts': [
+            'iterminal=iterminal:main',
+        ],
+    },
+)
